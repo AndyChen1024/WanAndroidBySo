40df3f81a5245d80dba12e8a187d7bf65b235387
sorrowdrug <sorrowdrug@sina.com>
2019-06-06 17:18:08 +0800

    初始化项目

---
+++ app/src/main/java/com/sorrowdrug/wanandroidbyso/MainActivity.java
@@ -0,0 +1,13 @@
+package com.sorrowdrug.wanandroidbyso;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+  }
+}
